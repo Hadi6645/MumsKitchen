2cd6df3b4c75092a7f14411043c7982dc4926c9d
Lydiaas <lidya13.10@gmail.com>
2021-02-03 16:32:29 +0200

    ..

--- src/main/java/control/DataService.java
+++ src/main/java/control/DataService.java
@@ -2,10 +2,33 @@ package control;
 
 import java.util.List;
 
+import entities.Complaint;
+import entities.ComplaintsReport;
+import entities.DeniedClientReprt;
+import entities.Employee;
+import entities.FoodOrderReport;
+import entities.Report;
 import entities.Reservation;
+import entities.Restaurant;
+import entities.UpdateMenuRequest;
 import entities.table;
 
 public class DataService {
+	
+	
+private static DataService DataService;
+	
+	private DataService() {
+		
+	}
+	public static DataService getDataService()
+	{
+		if (DataService == null) {
+			DataService = new DataService();
+		}
+		return DataService;
+	}
+	
 	
 	public boolean checReservationTables(Reservation res)
 	{
@@ -13,7 +36,7 @@ public class DataService {
 		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.CHECK_UNRESERVED_TABLES, data);
 		// send instruction to the server and get a response	
 		List<table> response = (List<table>)Connector.connectToServer(sInstruction);
-		return response.size() > 0;
+		return response!=null && response.size() > 0;
 	}
 
 	public int sendReservation(Reservation reserve)   // reservation complete 
@@ -22,6 +45,180 @@ public class DataService {
 		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.MAKE_RESERVATION, data);
 		// send instruction to the server and get a response	
 		int response = (int)Connector.connectToServer(sInstruction);
+		return response;
+	}
+	
+	
+	public List<List<table>> CanfirmReservation(Reservation res)  
+	{
+		Reservation data = res;
+		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.GET_RESERVATION_POSSIBILITIES, data);
+		// send instruction to the server and get a response	
+		List<List<table>> response = (List<List<table>>)Connector.connectToServer(sInstruction);
+		return response;
+	}
+	
+	
+	
+	public float[] CancelFoodOrder(int FoodOrderID)   // return money back and order price 
+	{
+		int data = FoodOrderID;
+		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.CANCEL_FOOD_ORDER, data);
+		// send instruction to the server and get a response	
+		float[] response = (float[])Connector.connectToServer(sInstruction);
+		return response;
+	}
+	
+	public int CancelReservation(int ReservationID)   // return penalty(10nis per person) (0 all is good)
+	{
+		int data = ReservationID;
+		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.CANCEL_RESERVATION, data);
+		// send instruction to the server and get a response	
+		int response = (int)Connector.connectToServer(sInstruction);
+		return response;
+	}
+	
+	public boolean AddComplaint(Complaint complaint)  
+	{
+		Complaint data = complaint;
+		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.ADD_COMPLAINT, data);
+		// send instruction to the server and get a response	
+		boolean response = (boolean)Connector.connectToServer(sInstruction);
+		return response;
+	}
+	
+	
+	//**//
+	
+	public List<Complaint> getActiveComplaint()   /// return list of active complaint to the employee
+	{
+		
+		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.GET_ACTIVE_COMPLAINTS, null);
+		// send instruction to the server and get a response	
+		List<Complaint> response = (List<Complaint>)Connector.connectToServer(sInstruction);
+		return response;
+	}
+	
+	
+	public boolean UpdateComplaint(Complaint complaint)  ///employee updates complaint status
+	{
+		Complaint data = complaint;
+		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.UPDATE_COMPLAINT, data);
+		// send instruction to the server and get a response	
+		boolean response = (boolean)Connector.connectToServer(sInstruction);
+		return response;
+	}
+	
+	public boolean Add_New_Update_Menu_Request(UpdateMenuRequest request)
+	{
+		UpdateMenuRequest data = request;
+		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.ADD_NEW_UPDATE_MENU_REQUEST, data);
+		// send instruction to the server and get a response	
+		boolean response = (boolean)Connector.connectToServer(sInstruction);
+		return response;
+		
+	}
+	public boolean Update_Menu_Request(UpdateMenuRequest updatemenu)  ///employee updates request status
+	{
+		UpdateMenuRequest data = updatemenu;
+		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.UPDATE_UPDATE_MENU_REQUEST, data);
+		// send instruction to the server and get a response	
+		boolean response = (boolean)Connector.connectToServer(sInstruction);
+		return response;
+	}
+	
+	public boolean Log_out(Employee employee)  ///log out
+	{
+		Employee data = employee;
+		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.LOG_OUT, data);
+		// send instruction to the server and get a response	
+		boolean response = (boolean)Connector.connectToServer(sInstruction);
+		return response;
+	}
+	
+	//reports
+	public DeniedClientReprt getDeniedReport(ReportRequest request )  //1
+	{
+		ReportRequest data = request;
+		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.GET_DENIED_CUSTOMERS_REPORT, data);
+		// send instruction to the server and get a response	
+		DeniedClientReprt response = (DeniedClientReprt)Connector.connectToServer(sInstruction);
+		return response;
+	}
+	
+	public List<DeniedClientReprt> getAllDeniedReport(ReportRequest request )  //2
+	{
+		ReportRequest data = request;
+		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.GET_ALL_DENIED_CUSTOMERS_REPORTS, data);
+		// send instruction to the server and get a response	
+		List<DeniedClientReprt> response = (List<DeniedClientReprt>)Connector.connectToServer(sInstruction);
+		return response;
+	}
+	
+	
+	public FoodOrderReport getFoodOrderReport(ReportRequest request )  //3
+	{
+		ReportRequest data = request;
+		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.GET_FOOD_ORDERS_REPORT, data);
+		// send instruction to the server and get a response	
+		FoodOrderReport response = (FoodOrderReport)Connector.connectToServer(sInstruction);
+		return response;
+	}
+	
+	public List<FoodOrderReport> getAlFoodOrdersReport(ReportRequest request )  //4
+	{
+		ReportRequest data = request;
+		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.GET_ALL_FOOD_ORDERS_REPORT, data);
+		// send instruction to the server and get a response	
+		List<FoodOrderReport> response = (List<FoodOrderReport>)Connector.connectToServer(sInstruction);
+		return response;
+	}
+	
+
+	public ComplaintsReport getComplaintReport(ReportRequest request )  //5
+	{
+		ReportRequest data = request;
+		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.GET_COMPLAINTS_REPORT, data);
+		// send instruction to the server and get a response	
+		ComplaintsReport response = (ComplaintsReport)Connector.connectToServer(sInstruction);
+		return response;
+	}
+	
+	public List<ComplaintsReport> getAllComplaintsReport(ReportRequest request )  //6
+	{
+		ReportRequest data = request;
+		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.GET_ALL_COMPLAINTS_REPORT, data);
+		// send instruction to the server and get a response	
+		List<ComplaintsReport> response = (List<ComplaintsReport>)Connector.connectToServer(sInstruction);
+		return response;
+	}
+	
+	
+	public List<Report> getRestaurantReports(ReportRequest request )  //7
+	{
+		ReportRequest data = request;
+		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.GET_RESTAURANT_REPORTS, data);
+		// send instruction to the server and get a response	
+		List<Report> response = (List<Report>)Connector.connectToServer(sInstruction);
+		return response;
+	}
+	
+	public List<List<Report>> getAllRestaurantReports(ReportRequest request )  //8
+	{
+		ReportRequest data = request;
+		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.GET_ALL_RESTAURANTS_REPORTS, data);
+		// send instruction to the server and get a response	
+		List<List<Report>> response = (List<List<Report>>)Connector.connectToServer(sInstruction);
+		return response;
+	}
+	
+	
+	public Restaurant getRestaurantByEmployee(String employeeID)  //9
+	{
+		String data = employeeID;
+		ServerInstruction sInstruction = new ServerInstruction(ServerInstructionType.GET_RESTAURANT_BY_EMPLOYEE, data);
+		// send instruction to the server and get a response	
+		Restaurant response = (Restaurant)Connector.connectToServer(sInstruction);
 		return response;
 	}
 }
--- src/main/java/userInterface/Cancelation.java
+++ src/main/java/userInterface/Cancelation.java
@@ -1,5 +1,46 @@
+/**
+ * Sample Skeleton for 'Cancelation.fxml' Controller Class
+ */
+
 package userInterface;
 
+import control.DataService;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+
 public class Cancelation {
+
+    @FXML // fx:id="food_order"
+    private CheckBox food_order; // Value injected by FXMLLoader
+
+    @FXML // fx:id="reservation"
+    private CheckBox reservation; // Value injected by FXMLLoader
+
+    @FXML // fx:id="cancel_id"
+    private TextField cancel_id; // Value injected by FXMLLoader
+
+    @FXML // fx:id="CANCEL"
+    private Button CANCEL; // Value injected by FXMLLoader
+    
+
+    private boolean isfoodordercancel = food_order.isSelected();
+    
+    @FXML
+    void cancelation(ActionEvent evsent) {
+    	DataService dat = DataService.getDataService();
+    			;
+    	if(isfoodordercancel ) {
+    		dat.CancelFoodOrder(Integer.parseInt(cancel_id));
+    	}
+    	else {
+    		
+    	}
+    	
+
+    }
 
 }
