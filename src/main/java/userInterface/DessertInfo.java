98f37dc7586c83cac22ff10522c5e65660628d4d
hind-abusaleh <hindabusaleh@gmail.com>
2021-02-03 16:05:10 +0200

    food order

--- src/main/java/userInterface/DessertInfo.java
+++ src/main/java/userInterface/DessertInfo.java
@@ -1,5 +1,143 @@
+/* Sample Skeleton for 'MealInfo.fxml' Controller Class
+ */
+
 package userInterface;
 
+import java.io.IOException;
+import java.util.List;
+
+import control.Cache;
+import entities.Ingredients;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
 public class DessertInfo {
+
+    @FXML // fx:id="descriptiontext"
+    private TextArea descriptiontext; // Value injected by FXMLLoader
+
+    @FXML // fx:id="base_list"
+    private ListView<Ingredients> base_list; // Value injected by FXMLLoader
+    private ObservableList<Ingredients> listViewData_base = FXCollections.observableArrayList();
+    
+    @FXML // fx:id="optinal_list"
+    private ListView<Ingredients> optinal_list; // Value injected by FXMLLoader
+    private ObservableList<Ingredients> listViewData_optinal = FXCollections.observableArrayList();
+    
+    
+    Cache cache = Cache.getCache();
+    Ingredients ingre ;
+    
+    @FXML
+    public void initialize() {
+    	descriptiontext.setText("Product: "+(cache.getCurrent_dessert().getID()-1) + "\n" + cache.getCurrent_dessert().getName() + "\n" + "Description: " + 
+    cache.getCurrent_dessert().getDesc() + "\n" + "Price: " + cache.getCurrent_dessert().getPrice());
+    	
+    	List<Ingredients> base = cache.getCurrent_dessert().getBaseIng();
+    	 for(int i=0; i<base.size();i++) {
+    		 listViewData_base.add(base.get(i));
+          }
+    	 
+    	// Init ListView.
+    	 base_list.setItems(listViewData_base);
+    	 base_list.setCellFactory((list) -> {
+             return new ListCell<Ingredients>() {
+                 @Override
+                 protected void updateItem(Ingredients item, boolean empty) {
+                     super.updateItem(item, empty);
+
+                     if (item == null || empty) {
+                         setText(null);
+                     } else {
+                         setText(item.getName());
+                     }
+                 }
+             };
+         });
+         
+       /*  
+      // Handle ListView selection changes.
+    	 base_list.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+         
+             System.out.println("ListView Selection Changed (selected: " + newValue + ")");
+            try {
+          	 
+  		} catch (IOException e) {
+  			// TODO Auto-generated catch block
+  			e.printStackTrace();
+  		}
+         });
+         */
+    	 
+    	 List<Ingredients> op = cache.getCurrent_meal().getOptIng();
+    	 for(int i=0; i<op.size();i++) {
+    		 listViewData_optinal.add(op.get(i));
+          }
+    	 
+    	// Init ListView.
+    	 optinal_list.setItems(listViewData_optinal);
+    	 optinal_list.setCellFactory((list) -> {
+             return new ListCell<Ingredients>() {
+                 @Override
+                 protected void updateItem(Ingredients item, boolean empty) {
+                     super.updateItem(item, empty);
+
+                     if (item == null || empty) {
+                         setText(null);
+                     } else {
+                         setText(item.getName());
+                     }
+                 }
+             };
+         });
+    	 
+    	 // Handle ListView selection changes.
+    	 base_list.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+         
+             System.out.println("ListView Selection Changed (selected: " + newValue + ")");
+            ingre = newValue;
+       	    
+         });
+    }
+    
+    
+    @FXML
+    void delete_op_fromeal(ActionEvent event) {
+    	for(int j =0; j< listViewData_optinal.size(); j++) {
+    		if(listViewData_optinal.get(j) == ingre) {
+    			listViewData_optinal.remove(j);
+    		}
+    	}
+    	// Init ListView.
+   	 optinal_list.setItems(listViewData_optinal);
+   	 optinal_list.setCellFactory((list) -> {
+            return new ListCell<Ingredients>() {
+                @Override
+                protected void updateItem(Ingredients item, boolean empty) {
+                    super.updateItem(item, empty);
+
+                    if (item == null || empty) {
+                        setText(null);
+                    } else {
+                        setText(item.getName());
+                    }
+                }
+            };
+        });
+    }
+
+    @FXML
+    void update_return(ActionEvent event) throws IOException {
+    	
+    	cache.getCurrent_dessert().setNewIng(listViewData_optinal);
+    	App.setRoot("Food_Order");
+
+    }
 
 }
--- src/main/java/userInterface/DrinkInfo.java
+++ src/main/java/userInterface/DrinkInfo.java
@@ -1,5 +1,43 @@
+/**
+ * Sample Skeleton for 'DrinkInfo.fxml' Controller Class
+ */
+
 package userInterface;
 
+import java.io.IOException;
+
+import control.Cache;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class DrinkInfo {
+
+    @FXML // fx:id="descriptiontext"
+    private TextArea descriptiontext; // Value injected by FXMLLoader
+    
+    Cache cache = Cache.getCache();
+
+    @FXML
+    void return_func(ActionEvent event) throws IOException {
+
+    	App.setRoot("Food_Order");
+    	
+    }
+    
+    @FXML
+    public void initialize() {
+    	
+    	if(cache.getCurrent_drink().getSparke() == true) {
+    	descriptiontext.setText("Product: "+(cache.getCurrent_drink().getID()-1) + "\n" + cache.getCurrent_drink().getName() + "\n" + "Description: " + 
+    cache.getCurrent_drink().getDesc() + "\n" + "Price: " + cache.getCurrent_drink().getPrice()+"\n" + "Alcohol Percent: " 
+    			+ String.valueOf(cache.getCurrent_drink().getalcPercent()) + "spark drink");
+    	}
+    	else {
+    		descriptiontext.setText("Product: "+(cache.getCurrent_drink().getID()-1) + "\n" + cache.getCurrent_drink().getName() + "\n" + "Description: " + 
+    			    cache.getCurrent_drink().getDesc() + "\n" + "Price: " + cache.getCurrent_drink().getPrice() +"\n"+ "Alcohol Percent: " 
+    			    			+ String.valueOf(cache.getCurrent_drink().getalcPercent()) );
+    	}
+    }
 
 }
